bb9d1f66f81af16e6bdaa2aa5136aa3181ba30b2
BloodDragon2580 <gewinner.thomas@gmail.com>
2019-10-30 05:11:04 +0100

    Update

---
+++ AutoInvitation/locale/ruRU.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AutoInvitation", "ruRU")
+if not L then return end
+
+L["auto-inviting"] = "|cffff00ff Теперь авто-приглашение."
+L["No-longer"] = "|cffff00ff Больше нет автоматического приглашения."
+L["Full-party"] = "|cff00ff00 Полная группа, больше нет автоматического приглашения."
